71a94d95c36996c5f29f7d164e77d6797694852f
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-10-07 23:13:21 -0400

    Create leetCode151.tsx

---
+++ leetcoding_Playground/src/leetCode_jsPlaygrounds/leetCode151.tsx
@@ -0,0 +1,19 @@
+function reverseWords (s:string):string{
+
+    // Problem - given a input string s, reverse the order of the words
+    // A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space. 
+
+    // Return a string of the words in reverse order concatenated by a single space
+
+    //Note that s may contain leading or trailing space or multiple spaces between two words. The returned string should only have a single space separating the words. Do no include any extra space. 
+
+    // Pseudo code: 
+
+    // we want to pass a string into our function and return a new string that reverses order of our words in our string.
+
+    // spaces will be trimmed only one space will exist between words. 
+
+    
+
+    return "";
+}
